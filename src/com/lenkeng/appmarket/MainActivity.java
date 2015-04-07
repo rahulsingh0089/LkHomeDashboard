0d9ca4f5d4adb9ea8815b2b560ebf901f41c2402
xgh <xgh@lenkeng.com>
2015-04-07 02:09:33 +0000

    修改应用详情界面三张缩略图不能选择的bug

--- src/com/lenkeng/appmarket/MainActivity.java
+++ src/com/lenkeng/appmarket/MainActivity.java
@@ -310,7 +310,7 @@ public class MainActivity extends Activity implements OnFocusChangeListener,
 	};
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) { 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
